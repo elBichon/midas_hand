c7bcdff9fc6f0ee575bf3760a0043ff3999e6ae6
elBichon <reuglewiczjeanedouard@gmail.com>
2019-11-12 22:58:21 +0100

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -20,6 +20,7 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import math
 from keras.models import load_model
+from sklearn.ensemble import IsolationForest
 
 # to not display the warnings of tensorflow
 import os
@@ -32,21 +33,6 @@ from keras.layers import LSTM
 from keras.layers import Dense
 
 
-# split a univariate sequence into samples
-def split_sequence(sequence, n_steps):
-	X, y = list(), list()
-	for i in range(len(sequence)):
-		# find the end of this pattern
-		end_ix = i + n_steps
-		# check if we are beyond the sequence
-		if end_ix > len(sequence)-1:
-			break
-		# gather input and output parts of the pattern
-		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
-		X.append(seq_x)
-		y.append(seq_y)
-	return array(X), array(y)
- 
 # split a univariate sequence into samples
 def split_sequence(sequence, n_steps):
 	X, y = list(), list()
@@ -115,26 +101,29 @@ print('LSTM model')
 print('==================')
 print('==================')
 
-
-
 # define input sequence
 raw_seq = fft_100_close[len(fft_100_close)-8:len(fft_100_close)]
 print(raw_seq)
+prediction = []#
 # choose a number of time steps
 n_steps = 7
+
+i = 0
+while i < 6:
 # split into samples
-X, y = split_sequence(raw_seq, n_steps)
+    X, y = split_sequence(raw_seq, n_steps)
 # define model
-model = Sequential()
-model.add(Dense(100, activation='relu', input_dim=n_steps))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
+    model = Sequential()
+    model.add(Dense(100, activation='relu', input_dim=n_steps))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
 # fit model
-model.fit(X, y, epochs=2000, verbose=0)
+    model.fit(X, y, epochs=2000, verbose=0)
 # demonstrate prediction
-x_input = array(fft_100_close[len(fft_100_close)-8:len(fft_100_close)-1])
-x_input = x_input.reshape((1, n_steps))
-yhat = model.predict(x_input, verbose=0).tolist()[0][0]
-print('predicted: ',yhat)
-print(type(yhat))
-print('expected: 115.16')
+    x_input = array(fft_100_close[len(fft_100_close)-8:len(fft_100_close)-1])
+    x_input = x_input.reshape((1, n_steps))
+    yhat = model.predict(x_input, verbose=0).tolist()[0][0]
+    raw_seq.append(yhat)
+    prediction.append(yhat)
+    i += 1
+print('predicted: ',prediction)
